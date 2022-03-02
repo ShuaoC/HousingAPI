49f49479241d2f15d5e9ecafea506019c7bc0826
ShuaoC <felix19981228@gmail.com>
2022-03-02 15:27:52 -0500

    Commit changes

--- src/main/java/com/example/HousingAPI/HousingAPIController.java
+++ src/main/java/com/example/HousingAPI/HousingAPIController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
@@ -27,13 +29,47 @@ public class HousingAPIController {
         return "Success";
     }
 
-    public String housingData() throws IOException {
-        int price;
-        int squareFeet;
+    public String housingData(String field, String statistic, Integer zipCode, String startDate, String endDate) throws IOException {
+        List<int[]> AllHousingData = new ArrayList<>();
+        int[] houseData = new int[4]; /*0 - price, 1 - sqt, 2 - date, 3 - zipcode*/
+        int minPrice;
+        int maxPrice;
+        int minSquareFeet;
+        int maxSquareFeet;
+        int averagePrice;
+        int averageSquareFeet;
+        int sumPrice;
+        int sumSquareFeet;
+        boolean firstLoop = true;
+
 
         Reader in = new FileReader("real-estate-data.csv");
         Iterable<CSVRecord> records = CSVFormat.RFC4180.parse(in);
 
+        for (CSVRecord record : records) {
+            if(firstLoop){
+                firstLoop = false;
+                continue;
+            }
+            houseData[0] = Integer.parseInt(record.get(9));
+            houseData[1] = Integer.parseInt(record.get(6));
+            houseData[2] = Integer.parseInt(record.get(8).substring(8,10));
+            houseData[3] = Integer.parseInt(record.get(2));
+
+            AllHousingData.add(houseData);
+
+        }
+
+        int rangePrice;
+        int rangeSquareFeet;
+
+        if(field.equals("price")){
+            if(statistic)
+        }else if(field.equals("statistic")){
+
+        }else{
+            return "Please enter the correct parameter";
+        }
 
         return "";
     }
--- src/main/java/com/example/HousingAPI/HousingApiApplication.java
+++ src/main/java/com/example/HousingAPI/HousingApiApplication.java
@@ -8,14 +8,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.Date;
 
 @SpringBootApplication
 public class HousingApiApplication {
 
 	public static void main(String[] args) throws IOException {
+		String city;
+		boolean firstLoop = true;
 		SpringApplication.run(HousingApiApplication.class, args);
 		Reader in = new FileReader("real-estate-data.csv");
 		Iterable<CSVRecord> records = CSVFormat.RFC4180.parse(in);
+		for (CSVRecord record : records){
+			if(firstLoop){
+				firstLoop = false;
+				continue;
+			}
+			city = record.get(8);
+			System.out.print(city.substring(8,10));
+		}
+
+	}
+
+	public int x(){
+		int y = 0;
+		return y;
 	}
 
 }
